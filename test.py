865b9448ea23960c315c7c1511ab8795d8fe874e
KunnuSherry <gamingvenom465@gmail.com>
2025-07-13 01:28:58 +0530

    C20 - Doing some final changes before prefinal deploy

--- test.py
+++ test.py
@@ -4,3 +4,16 @@ password = "adminofthestuddybuddyy69283123"
 hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 print(hashed.decode())  # This will give a string to store in .env
+
+
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config( 
+  cloud_name = "dwbgl0h17", 
+  api_key = "126572313438464", 
+  api_secret = "v-LXJgv3TZ1Fsm09QgOv3uh2gFA" 
+)
+
+result = cloudinary.uploader.upload("./static/avatars/image.png")
+print(result["secure_url"])
